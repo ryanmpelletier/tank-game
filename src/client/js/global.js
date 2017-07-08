4eff582a4de8b0bb641a46eec6588f936687a3a3
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2017-07-08 14:47:11 -0400

    Fixing all my stupid crap. Background works now.

--- src/client/js/global.js
+++ src/client/js/global.js
@@ -4,9 +4,5 @@ module.exports = {
     '65': 'KEY_LEFT',
     '87': 'KEY_UP',
     '68': 'KEY_RIGHT',
-    '83': 'KEY_DOWN',
-    'background_props':{
-        'cellHeight':50,
-        'cellWidth':50,
-    }
+    '83': 'KEY_DOWN'
 };

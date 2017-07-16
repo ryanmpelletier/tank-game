968fcba5a9ceae6c838a3a6ba0a259a3b216576f
Davis DeRuiter <dnd@DND-MacBook-Pro.local>
2017-07-16 14:26:53 -0400

    Changed from function to class.

--- src/client/js/tankSprite.js
+++ src/client/js/tankSprite.js
@@ -1,49 +1,67 @@
-exports.createSprite = function(options) {
-	
-    var that = {},
-        frameIndex = 0,
-        tickCount = 0,
-        ticksPerFrame = options.ticksPerFrame || 0,
-        numberOfFrames = options.numberOfFrames || 1;
-    
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-    
-    that.update = function () {
-        tickCount += 1;
+class TankSprite {
+    constructor(options){
+        this.width = options.width;
+        this.height = options.height;
+        this.numberOfFrames = options.numberOfFrames || 1;
+        this.ticksPerFrame = options.ticksPerFrame || 0;
 
-        if (tickCount > ticksPerFrame) {
-            tickCount = 0;
+        this.frameIndex = 0;
+        this.tickCount = 0;
+
+        this.context = options.context;
+
+        this.image = new Image();
+        this.image.src = "/img/tank-sprite.png";
+    }
+
+    update() {
+        this.tickCount += 1;
+
+        if (this.tickCount > this.ticksPerFrame) {
+            this.tickCount = 0;
             
             // If the current frame index is in range
-            if (frameIndex < numberOfFrames - 1) {	
+            if (this.frameIndex < this.numberOfFrames - 1) {
                 // Go to the next frame
-                frameIndex += 1;
-            } else {
+                this.frameIndex += 1;
+            }
+            else {
                 // Reset to first frame
-                frameIndex = 0;
+                this.frameIndex = 0;
             }
         }
     };
 		
-    that.render = function (destX, destY) {
-        // Clear the canvas
-        // that.context.clearRect(0, 0, that.width, that.height);
-        
-        // Draw the animation
-        that.context.drawImage(
-        that.image,
-        frameIndex * that.width / numberOfFrames,
-        0,
-        that.width / numberOfFrames,
-        that.height,
-        destX,
-        destY,
-        that.width / numberOfFrames,
-        that.height);
+    render(destX, destY, hullDirection) {
+        // Save current context to prevent rotating everything drawn after this occurs
+        this.context.save();
+
+        // Move registration point to tank's position
+        this.context.translate(destX, destY);
+
+        // Rotate entire canvas desired amount for tank's hull to rotate
+        // (rotates around axis at tank's current location)
+        this.context.rotate(hullDirection * Math.PI / 180);
+
+        // Move registration point back to the top left corner of canvas
+        // (necessary otherwise tank is drawn at wrong location since canvas has been rotated)
+        this.context.translate(-destX, -destY);
+
+        // Draw tank sprite at destination coordinates
+        this.context.drawImage(
+            this.image,
+            this.frameIndex * this.width / this.numberOfFrames,
+            0,
+            this.width / this.numberOfFrames,
+            this.height,
+            destX - 64,
+            destY - 64,
+            this.width / this.numberOfFrames,
+            this.height
+        );
+
+        this.context.restore();
     };
-    
-    return that;
 }
+
+module.exports = TankSprite;

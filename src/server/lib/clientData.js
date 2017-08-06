cca4b6e895e02e2a0558f92a41bd9a89a1dd779c
Davis DeRuiter <dnd@DND-MacBook-Pro.local>
2017-08-06 17:34:26 -0400

    Indented code using a uniform indentation level of 4 spaces for easier readability.

--- src/server/lib/clientData.js
+++ src/server/lib/clientData.js
@@ -10,50 +10,55 @@ var Tank = require('./tank');
 var Direction = require('./direction');
 
 class ClientData {
-  constructor(id, startXPosition, startYPosition) {
-    //stuff the client gives us
-    this.player = {
-      screenName: undefined,
-      screenWidth: undefined,
-      screenHeight: undefined,
-      userInput:{
-        keysPressed:{}
-      }
-    };
-    //stuff the client does NOT give us or ever set, this is read by the server for game logic/managing
-    this.id = id;
-    this.ping = undefined;
-    this.startPingTime = undefined;
-    this._position = {
-      x: startXPosition,
-      y: startYPosition
-    };
-    
-    this.lastHeartbeat = new Date().getTime();
+    constructor(id, startXPosition, startYPosition) {
+        //stuff the client gives us
+        this.player = {
+            screenName: undefined,
+            screenWidth: undefined,
+            screenHeight: undefined,
+            userInput: {
+                keysPressed:{}
+            }
+        };
 
-    //should probably make a setter for x and y which also changes the tank's x and y
-    this.tank = new Tank(this.id, this.position.x, this.position.y);
-    /**
-     * simple quadtree requires a basic format for object put onto the quadtree, I am trying to figure out the best way to mitigate this
-     * I don't like a libary enforcing my object to have a certain structure, this is something I am not used to. In Java this would just be
-     * an interface I would implement, and I wouldn't have to change the internal representation of my object, this is my compromise
-     */
-    this.forQuadtree = function(){
-        return this.tank.forQuadtree();
-    };
-  }
+        //stuff the client does NOT give us or ever set, this is read by the server for game logic/managing
+        this.id = id;
+        this.ping = undefined;
+        this.startPingTime = undefined;
+        this._position = {
+            x: startXPosition,
+            y: startYPosition
+        };
 
-  set position (position){
-    //set tank position also
-    this.tank.x = position.x;
-    this.tank.y = position.y;
-    this.tank.addLocation({x: position.x, y: position.y});
-    this._position = position;
-  }
+        this.lastHeartbeat = new Date().getTime();
 
-  get position (){
-    return this._position;
-  }
+        //should probably make a setter for x and y which also changes the tank's x and y
+        this.tank = new Tank(this.id, this.position.x, this.position.y);
+
+        /**
+         * simple quadtree requires a basic format for object put onto the quadtree, I am trying to figure out the best way to mitigate this
+         * I don't like a libary enforcing my object to have a certain structure, this is something I am not used to. In Java this would just be
+         * an interface I would implement, and I wouldn't have to change the internal representation of my object, this is my compromise
+         */
+        this.forQuadtree = function() {
+            return this.tank.forQuadtree();
+        };
+    }
+
+    set position (position) {
+        //set tank position also
+        this.tank.x = position.x;
+        this.tank.y = position.y;
+        this.tank.addLocation({
+            x: position.x,
+            y: position.y
+        });
+        this._position = position;
+    }
+
+    get position () {
+        return this._position;
+    }
 
 }
 

3f5c400a6d5a117988f3fe527e8513a4b67f30f9
Davis DeRuiter <dnd@DND-MacBook-Pro.local>
2017-08-22 22:29:33 -0400

    Removed unnecessary forQuadtree() function. It was simply calling the Tank class forQuadtree() function, so we can just call this directly.

--- src/server/lib/clientData.js
+++ src/server/lib/clientData.js
@@ -31,14 +31,6 @@ class ClientData {
 
         //should probably make a setter for x and y which also changes the tank's x and y
         this.tank = new Tank(this.id, this.position.x, this.position.y);
-        /**
-         * simple quadtree requires a basic format for object put onto the quadtree, I am trying to figure out the best way to mitigate this
-         * I don't like a libary enforcing my object to have a certain structure, this is something I am not used to. In Java this would just be
-         * an interface I would implement, and I wouldn't have to change the internal representation of my object, this is my compromise
-         */
-        this.forQuadtree = function() {
-            return this.tank.forQuadtree();
-        };
     }
 
     set position (position) {
